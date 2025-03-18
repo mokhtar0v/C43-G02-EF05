d222b83bd935dd04ffaa3465122b795b1709a184
mokhtar0v <Omar.Mokhtar22@svnu.edu.eg>
2025-03-18 03:13:54 +0200

    Stored Procedure

--- DatabaseFirstProject/Program.cs
+++ DatabaseFirstProject/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace DatabaseFirstProject
+﻿using DatabaseFirstProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatabaseFirstProject
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            using NorthwindDbContext context = new NorthwindDbContext();
+
+            int count = 3;
+            var res = context.Categories.FromSqlRaw("select top(3)*\r\nfrom Categories");
+            var categories = context.Categories.FromSqlInterpolated($"select top({count})*\r\nfrom Categories");
+
+            var catID = 1;
+            context.Database.ExecuteSqlInterpolated($"update Categories\r\nset CategoryName = 'TestCat'\r\nwhere CategoryID = {catID}");
+
         }
     }
 }
