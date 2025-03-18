88d02a3447cd077cedf44777371af3e66ade84a2
mokhtar0v <Omar.Mokhtar22@svnu.edu.eg>
2025-03-18 02:10:48 +0200

    Part03

---
+++ DatabaseFirstProject/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DatabaseFirstProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
